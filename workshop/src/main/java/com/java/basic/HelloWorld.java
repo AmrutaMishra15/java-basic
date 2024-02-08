0c2b8bda16031bffe11745d492fecfd838c4d992
amrut <amrut@host.docker.internal>
2024-02-08 00:05:41 -0500

    initial commit

---
+++ workshop/src/main/java/com/java/basic/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.java.basic;
+
+//Basic Java program with File name: HelloWorld.java
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// The program begins with a call to main() method. 
+		// Prints Output  "Hello, World" 
+		System.out.println("Hello, World"); 
+	}
+
+}
