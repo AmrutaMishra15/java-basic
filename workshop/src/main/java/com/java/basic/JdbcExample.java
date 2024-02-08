8930a46b3c0a2574a6ad7c02f6fd0015b48f6c1e
amruta <amrut@Amruta>
2024-02-08 16:18:50 -0500

    jdbc example

---
+++ workshop/src/main/java/com/java/basic/JdbcExample.java
@@ -0,0 +1,30 @@
+package com.java.basic;
+
+import java.sql.*;
+
+public class JdbcExample {
+
+	static final String DB_URL = "jdbc:mysql://localhost/test";
+	static final String USER = "guest";
+	static final String PASS = "guest123";
+	static final String QUERY = "SELECT id, first, last, age FROM Test";
+
+	public static void main(String[] args) {
+		try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(QUERY);) {
+			// Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				System.out.print("ID: " + rs.getInt("id"));
+				System.out.print(", Age: " + rs.getInt("age"));
+				System.out.print(", First: " + rs.getString("first"));
+				System.out.println(", Last: " + rs.getString("last"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
